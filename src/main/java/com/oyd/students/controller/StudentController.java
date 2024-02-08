713d7f0c0c1e96f900def8234f855415137858ba
Vangjel <vfrasheri21@gmail.com>
2024-02-08 12:16:48 -0500

    new endpoints to add a student via post request. also added new search by first and last name

--- src/main/java/com/oyd/students/controller/StudentController.java
+++ src/main/java/com/oyd/students/controller/StudentController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +27,14 @@ public class StudentController {
 		return studentRepo.findAll();
 	}
 	
+	@PostMapping("/addStudent")
+	public Student addStudent(@RequestBody Student student) {
+		return studentRepo.save(student);
+	}
+	
+	@GetMapping("/student/{first}/{last}")
+	public List<Student> getStudentByName(@PathVariable String first, @PathVariable String last) {
+		return studentRepo.findByFirstNameAndLastName(first, last);
+		
+	}
 }
